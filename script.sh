4ee4ef4a2765018217597eeba40da8208f8bc9d1
Shady-brain <64219178+Shady-brain@users.noreply.github.com>
2022-11-10 22:50:49 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 sudo apt install neofetch xinit xcompmgr openbox nitrogen flatpak firefox pcmanfm lxtask lxappearance xfce4-panel xfce4-whiskermenu-plugin pop-icon-theme volumeicon-alsa alsa-base lxterminal openbox-menu -y &&
-sudo apt install vlc obs-studio libreoffice gimp -y 
+sudo apt install vlc obs-studio libreoffice gimp rhythmbox gnome-calculator -y 
 wget https://images2.alphacoders.com/127/thumb-1920-1270806.jpg &&
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
 flatpak install com.discordapp.Discord org.kde.krita com.spotify.Client com.valvesoftware.Steam flathub com.visualstudio.code -y &&
