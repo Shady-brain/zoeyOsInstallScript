1372c7c2a4df9ee9d70519169c70a3d265e74dfd
Shady-brain <64219178+Shady-brain@users.noreply.github.com>
2022-11-10 22:37:30 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
-sudo apt install neofetch xinit xcompmgr openbox nitrogen flatpak firefox pcmanfm lxtask lxappearance xfce4-panel xfc4-whiskermenu-plugin pop-icon-theme volumeicon-alsa alsa lxterminal openbox-menu -y &&
+sudo apt install neofetch xinit xcompmgr openbox nitrogen flatpak firefox pcmanfm lxtask lxappearance xfce4-panel xfce4-whiskermenu-plugin pop-icon-theme volumeicon-alsa alsa-base lxterminal openbox-menu -y &&
 sudo apt install vlc obs-studio libreoffice gimp -y 
 wget https://images2.alphacoders.com/127/thumb-1920-1270806.jpg &&
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo &&
 flatpak install com.discordapp.Discord org.kde.krita com.spotify.Client com.valvesoftware.Steam flathub com.visualstudio.code -y &&
 cd ~/.config/ && mkdir openbox && cd ./openbox && touch autostart.sh && echo "pcmanfm --dekstop & nitrogen --restore & xfc4-panel & lxterminal & volumeicon & xcompmgr & " &&
 curl -L https://nixos.org/nix/install | sh && nix-env -i hyfetch &&
-do-release-update &&
+do-release-update -q &&
+echo "done"
