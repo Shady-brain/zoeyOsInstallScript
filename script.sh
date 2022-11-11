cb8d2eb87fd5185cfcb55465f61b41d4a8b15a82
Shady-brain <64219178+Shady-brain@users.noreply.github.com>
2022-11-11 17:09:34 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 sudo apt-get install software-properties-common -y 
 sudo apt-get update 
-sudo apt install neofetch xinit xcompmgr openbox nitrogen flatpak firefox pcmanfm lxtask lxappearance xfce4-panel xfce4-whiskermenu-plugin pop-icon-theme volumeicon-alsa alsa-base lxterminal openbox-menu -y 
+sudo apt install neofetch xinit xcompmgr openbox nitrogen flatpak firefox pcmanfm lxtask lxappearance xfce4-panel xfce4-whiskermenu-plugin pop-icon-theme volumeicon-alsa alsa-base lxterminal openbox-menu xfce4-clipman-plugin xfce4-battery-plugin xfce4-mpc-plugin xfce4-wavelan-plugin-y 
 sudo apt install vlc obs-studio libreoffice gimp rhythmbox gnome-calculator -y
 wget https://images2.alphacoders.com/127/thumb-1920-1270806.jpg
 #flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
