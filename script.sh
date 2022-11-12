bed015aaf88e8b56b26c4161f49c0a6bf633d7dc
Zoey Ahmed <64219178+Shady-brain@users.noreply.github.com>
2022-11-12 18:10:32 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,8 @@
 sudo apt-get install software-properties-common -y 
 sudo apt-get update 
 sudo apt curl install neofetch xinit xcompmgr openbox nitrogen flatpak firefox pcmanfm lxtask lxappearance xfce4-panel xfce4-whiskermenu-plugin pop-icon-theme volumeicon-alsa alsa-base lxterminal openbox-menu xfce4-clipman-plugin xfce4-battery-plugin xfce4-mpc-plugin xfce4-wavelan-plugin mpd xfce4-settings -y 
-sudo apt install thunderbird vlc obs-studio libreoffice gimp rhythmbox gnome-calculator -y
+sudo apt install thunderbird vlc obs-studio libreoffice gimp rhythmbox gnome-calculator gedit nautilus -y
+cd ..
 mkdir ".wallpaper" 
 cd ~/.wallpaper
 wget https://images2.alphacoders.com/127/thumb-1920-1270806.jpg 
@@ -18,10 +19,15 @@ wget https://images.squarespace-cdn.com/content/v1/5bc9b8b27a1fbd76c82c7865/1615
 wget https://t3.ftcdn.net/jpg/04/61/32/18/360_F_461321820_VxV6rrZlMXCIHkl28UF0SkHea0ctgtge.jpg
 wget https://wallpapercave.com/uwp/uwp1531283.jpeg
 wget https://i.ytimg.com/vi/DfPDhvNjJZg/maxresdefault.jpg
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo 
 sudo flatpak install com.discordapp.Discord org.kde.krita com.spotify.Client com.valvesoftware.Steam flathub com.visualstudio.code com.mastermindzh.tidal-hifi -y 
 cd ~/.config/ && mkdir openbox && cd ./openbox && touch autostart.sh && echo "pcmanfm --desktop & nitrogen --restore & xfce4-panel & lxterminal & volumeicon & xcompmgr & " > ./autostart.sh
 #curl -L https://nixos.org/nix/install | sh && nix-env -i hyfetch 
 sudo add-apt-repository ppa:agornostal/ulauncher && sudo apt update && sudo apt install ulauncher
+sudo add-apt-repository --remove ppa:nemh/systemback
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 382003C2C8B7B4AB813E915B14E4942973C62A1B
+sudo add-apt-repository "deb http://ppa.launchpad.net/nemh/systemback/ubuntu xenial main"
+sudo apt update
+sudo apt install systemback
 do-release-upgrade -q 
 echo "done"
